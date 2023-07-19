ae7e19f096d5fbea081a4b447906c19781276978
Montodel <suleimanmoha120@gmail.com>
2023-07-19 18:04:55 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,47 +1,34 @@
 #!/usr/bin/env python3
-"""
-In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:).
-The core of the function is very simple.
-It uses the requests module to obtain
-the HTML content of a particular URL and returns it.
+""" expiring web cache module """
 
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the
-result with an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to
-simulate a slow response and test your caching.
-
-Bonus: implement this use case with decorators.
-"""
-from functools import wraps
-import requests as req
 import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+redis = redis.Redis()
 
 
-redis_cache = redis.Redis()
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
 
 
-def count_url_access(method):
-    """counts the no of times a url is accessed"""
-    @wraps(method)
-    def count(url):
-        url_key = url
-        cached_data = redis_cache.get(url_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = 'count:{}'.format(url)
-        html = method(url)
-        redis_cache.incr(count_key)
-        redis_cache.setex(url_key, 10, html)
-        return html
-    return count
-
-
-@count_url_access
+@wrap_requests
 def get_page(url: str) -> str:
-    """requests a url and returns the HTML content"""
-    html = req.get(url)
-    return html.text
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
