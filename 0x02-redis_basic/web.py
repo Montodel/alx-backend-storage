33dbdd9d92feda47fa66a6e626841f3130e06931
Montodel <suleimanmoha120@gmail.com>
2023-07-19 18:08:41 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-"""web module
-"""
+""" Module for Implementing an expiring web cache and tracker """
+
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
 
 r = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """
-    count requests decorate
-    """
+    """ Decortator for counting how many times a request
+    has been made """
+
     @wraps(method)
     def wrapper(url):
-        """
-        Wrapper for decorator functionality
-        """
+        """ Wrapper for decorator functionality """
         r.incr(f"count:{url}")
-        cache_html = r.get(f"cached:{url}")
-        if cache_html:
-            return cache_html.decode('utf-8')
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
         html = method(url)
         r.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
     """
-    get page method
-    """
-    request = requests.get(url)
-    return request.text
+    req = requests.get(url)
+    return req.text
